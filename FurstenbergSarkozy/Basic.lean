5d3012ad36420d72e52934ddc131a0f916a4a3f6
Gabriel Dahia <gdahia@users.noreply.github.com>
2024-10-12 18:07:15 -0300

    Prove delta uniform function inequalities
    commit ecab0ed57aa40955375c3dd418ca6dcee1216fe4
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sat Oct 12 18:05:59 2024 -0300
    
        Complete proof about delta uniform functions
    
    commit 137d4dca60459911f957906592f76b8261c0dd90
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sat Oct 12 18:04:17 2024 -0300
    
        Replace plain simps with simp? output
    
    commit 6307160b9f66dbe09faf379dd8d774ba3bb35ce3
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sat Oct 12 11:51:45 2024 -0300
    
        Replace almost proof with simpler one
    
        Co-authored-by: Mauricio Collares <mauricio@collares.org>
    
    commit f6f2e0c44a9cc6e7c89af57744a5616f9f8aad01
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Mon Oct 7 08:29:12 2024 -0300
    
        Finish proving reductions
    
    commit 3bf9c14b44ee3265c5d4d6129c412b14a76014d9
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sun Oct 6 18:42:27 2024 -0300
    
        Simplify rws
    
    commit 40db8457d3891c357665db19570991e2c12c28e7
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sun Oct 6 18:39:04 2024 -0300
    
        (Almost) prove major reduction
    
    commit 9ed1086d463cd1c9c9c6fc3c2b085b24ab80261c
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sun Oct 6 18:38:47 2024 -0300
    
        Fix definition of inner term
    
    commit d2f23967ccc74a42f928bd23e03df279173d4aa8
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sun Oct 6 17:46:08 2024 -0300
    
        Make proof shorter, use suffices a lot
    
    commit f3538eefd1d4a5b9820ba8bb2a7c83a52644669b
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sun Oct 6 17:45:30 2024 -0300
    
        Avoid trivial square differences
    
    commit caf2479f1a3feffc0b0df99699410c33c80ad272
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sun Oct 6 17:44:49 2024 -0300
    
        Use more mathlib-friendly definitions
    
    commit 0766fe2dd53a4416c5dfc6366d359aca4b9fa494
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sat Oct 5 12:59:35 2024 -0300
    
        Add new defs
    
    commit b8f733f28a658a4acc8d114881f016be699daa2f
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Fri Sep 27 21:26:11 2024 -0300
    
        Fix δ not being (0, 1], use proper argument ordering
    
    commit bdeb22e94c56e52690e86340858de39bc9d82687
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Fri Sep 27 21:25:50 2024 -0300
    
        Define containsSquareDifference in Finset
    
    commit 132f12e0ab0b198e950d67fefc1f7abbd59e4e84
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Fri Sep 27 21:19:36 2024 -0300
    
        Make some progress and improve definitions
    
    commit 262fd17a4aac2f2d667e7c3ea2b97743ee97cffc
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Thu Sep 26 14:59:57 2024 -0300
    
        WIP

--- FurstenbergSarkozy/Basic.lean
+++ FurstenbergSarkozy/Basic.lean
@@ -1,49 +1,140 @@
 import Mathlib
 
-open Finset
+open Finset Function
 
-noncomputable def generalizedCountOfSquares (n : ℕ) (f₁ f₂ : ℕ → unitInterval) : ℝ :=
-  ∑ x ∈ (range n),
-  ∑ r ∈ range (⌈(n ^ (1/3 : ℝ) : ℝ)⌉₊),
-  ∑ h ∈ range (⌈(n ^ (1/100 : ℝ) : ℝ)⌉₊), (f₁ x) * (f₂ (x + (r + h)^2))
+def range' (n : ℕ) : Finset ℕ := Ioc 0 n
 
-noncomputable def countOfSquares (n : ℕ) (f : ℕ → unitInterval) : ℝ :=
+noncomputable def upperBoundOny (n : ℕ) := ⌈(n ^ (1/3 : ℝ) : ℝ)⌉₊
+
+noncomputable def upperBoundOnz (n : ℕ) := ⌈(n ^ (1/100 : ℝ) : ℝ)⌉₊
+
+noncomputable def generalizedCountOfSquares (n : ℕ) (f₁ f₂ : ℕ → ℝ) : ℝ :=
+  ∑ x ∈ range' n,
+  ∑ y ∈ range' (upperBoundOny n),
+  ∑ z ∈ range' (upperBoundOnz n), (f₁ x) * (f₂ (x + (y + z)^2))
+
+noncomputable def countOfSquares (n : ℕ) (f : ℕ → ℝ) : ℝ :=
   generalizedCountOfSquares n f f
 
-def containsSquareDifference (S : Finset ℕ) : Prop := ∃ d : ℕ, ∃ a ∈ S, a + d ^ 2 ∈ S
+def Finset.containsSquareDifference (S : Finset ℕ) : Prop :=
+  ∃ d : {d : ℕ // d > 0}, ∃ a ∈ S, a + d ^ 2 ∈ S
 
-def id' {α : Type} (x : unitInterval) : α → unitInterval := fun _ => x
-def Finset.indicator {α : Type} [DecidableEq α] (S : Finset α) : α → unitInterval :=
-  S.piecewise (id' 1) (id' 0)
+noncomputable def Finset.indicator {α : Type} [DecidableEq α] (S : Finset α) : α → ℝ :=
+  S.toSet.indicator (const α 1)
 
-lemma square_difference_free_set_has_zero_countOfSquares (n : ℕ) (S : Finset ℕ)
-    (squareDifferenceFree : ¬ containsSquareDifference S) :
-    countOfSquares n S.indicator = 0 := by
+lemma non_zero_countOfSquares_implies_squareDifference (n : ℕ) (S : Finset ℕ)
+     : countOfSquares n S.indicator ≠ 0 → S.containsSquareDifference := by
+  contrapose!
+  intro squareDifferenceFree
 
   unfold containsSquareDifference at squareDifferenceFree
   push_neg at squareDifferenceFree
-  unfold countOfSquares generalizedCountOfSquares
+  unfold countOfSquares generalizedCountOfSquares range'
 
-  refine sum_eq_zero ?_
+  apply sum_eq_zero
   intros x _
-  refine sum_eq_zero ?_
-  intros r _
-  refine sum_eq_zero ?_
-  intros h _
+  apply sum_eq_zero
+  intros y hy
+  apply sum_eq_zero
+  intros z hz
 
   by_cases (x ∈ S)
   case neg xNotInS =>
     unfold indicator
-    simp
+    simp only [const_one, mul_eq_zero, Set.indicator_apply_eq_zero, mem_coe, Pi.one_apply,
+      one_ne_zero, imp_false]
     left
-    exact if_neg xNotInS
+    exact xNotInS
   case pos xInS =>
     unfold indicator
-    simp
+    simp only [const_one, mul_eq_zero, Set.indicator_apply_eq_zero, mem_coe, Pi.one_apply,
+      one_ne_zero, imp_false]
     right
-    exact if_neg (squareDifferenceFree (r + h) x xInS)
+    simp at hy
+    simp at hz
+    have ypz_pos : 0 < y + z := by omega
+    exact (squareDifferenceFree ⟨ y + z, ypz_pos ⟩ x xInS)
+
+noncomputable def maxOfGeneralizedCountOfSquares (n₁ n₂ n₃ : ℕ) :=
+  n₁ * (upperBoundOny n₂) * (upperBoundOnz n₃)
+noncomputable def maxOfCountOfSquares (n : ℕ) := maxOfGeneralizedCountOfSquares n n n
+noncomputable def almostN (n : ℕ) := n - (upperBoundOny n + upperBoundOnz n)^2
+noncomputable def almostMaxOfCountOfSquares (n : ℕ) :=
+  maxOfGeneralizedCountOfSquares (almostN n) n n
+
+lemma uniform_δ_indicator_at_least_sqr_δ_density_of_countOfSquares_minus_error
+    {n : ℕ} {δ : ℝ}
+    (n_is_large : (upperBoundOny n + upperBoundOnz n)^2 ≤ n) :
+    δ ^ 2 * (almostMaxOfCountOfSquares n) ≤ countOfSquares n (δ • (range' n).indicator) := by
+  simp only [countOfSquares, generalizedCountOfSquares, range', indicator,
+              Pi.smul_apply, smul_eq_mul, coe_Ioc]
+  have almost_sub : Ioc 0 (almostN n) ⊆ Ioc 0 n := by
+    simp only [almostN, Ioc_subset_Ioc_right, Nat.sub_le]
+  refine le_trans (le_of_eq ?_) (sum_le_sum_of_subset_of_nonneg almost_sub ?_)
+  · rw [sum_eq_card_nsmul]
+    rotate_left
+    · intro a ha
+      rw [sum_eq_card_nsmul]
+      intro y hy
+      rw [sum_eq_card_nsmul (b := δ * δ)]
+      intro z hz
+      have cond : a + (y + z) ^ 2 ∈ Set.Ioc 0 n := by
+        simp only [mem_Ioc, Set.mem_Ioc, add_pos_iff] at ha hy hz ⊢
+        refine ⟨Or.inl ha.1, ?_⟩
+        dsimp only [almostN] at ha
+        have hyz : (y + z) ^ 2 ≤ (upperBoundOny n + upperBoundOnz n) ^ 2 := by
+          nlinarith only [hy.2, hz.2]
+        simpa only [tsub_add_cancel_of_le n_is_large] using add_le_add ha.2 hyz
+      have ha : a ∈ Set.Ioc 0 n := by simpa only [mem_Ioc] using almost_sub ha
+      rw [Set.indicator_of_mem ha _, Set.indicator_of_mem cond _]
+      simp only [const_apply, mul_one]
+    · simp only [almostMaxOfCountOfSquares, maxOfGeneralizedCountOfSquares,
+        Nat.cast_mul, Nat.card_Ioc, tsub_zero]
+      ring
+  · intro a _ _
+    apply sum_nonneg
+    intro y _
+    apply sum_nonneg
+    intro z _
+    ring_nf
+    refine mul_nonneg (mul_nonneg (sq_nonneg δ) ?_) ?_ <;>
+      exact Set.indicator_nonneg (by simp) _
+
+lemma uniform_δ_indicator_at_most_sqr_δ_density_of_countOfSquares
+    (n : ℕ) (δ : ℝ) :
+    countOfSquares n (δ • (range' n).indicator) ≤ δ ^ 2 * (maxOfCountOfSquares n) := by
+  simp only [countOfSquares, maxOfCountOfSquares, maxOfGeneralizedCountOfSquares,
+              generalizedCountOfSquares, indicator, range', coe_Ioc, const_one,
+              Pi.smul_apply, smul_eq_mul, Nat.cast_mul, Set.indicator, Set.mem_Ioc,
+              Pi.one_apply, mul_ite, mul_one, mul_zero, add_pos_iff, ite_mul, zero_mul,
+              ←ite_and]
+  apply le_trans
+  · apply sum_le_card_nsmul
+    intro x hx
+    simp only [mem_Ioc] at hx
+    simp only [hx, true_or, true_and, and_self, and_true]
+    apply sum_le_card_nsmul
+    intro y _
+    apply sum_le_card_nsmul
+    intro z _
+    apply ite_le_sup (δ * δ) 0
+  · simp only [Nat.card_Ioc, tsub_zero, nsmul_eq_mul]
+    ring_nf
+    norm_cast
+    apply le_of_eq
+    simp only [Nat.cast_mul, mul_eq_mul_left_iff, sup_eq_left, mul_eq_zero, Nat.cast_eq_zero]
+    left
+    exact sq_nonneg δ
+
+-- approach should be the following: do cases. if we have not many fewer than
+-- expected square differences in S, then we are done. otherwise, we can go to a
+-- denser subset by the density increment argument and we show that recursively
+-- calling furstenberg_sarkozy terminates because δ increases and is at most one
+
+noncomputable def unitInterval' := (Set.Ioc (0 : ℝ) (1 : ℝ))
 
 theorem furstenberg_sarkozy :
-    ∀ α : ℝ, ∃ n₀ : ℕ, ∀ n : ℕ, ∀ S ⊆ (range n),
-    n ≥ n₀ ∧ S.card ≥ α * n → ∃ d : ℕ, ∃ a ∈ S, a + d ^ 2 ∈ S :=
+    ∀ δ ∈ unitInterval', ∃ n₀ : ℕ, ∀ n : ℕ, n ≥ n₀ →
+    ∀ S ⊆ (range' n), δ * n ≤ S.card →
+    S.containsSquareDifference :=
   sorry
