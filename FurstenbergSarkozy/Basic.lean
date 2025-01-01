29734123dfff6569c8c92e6c58c366d81828ade2
Gabriel Dahia <gdahia@users.noreply.github.com>
2025-01-01 10:00:42 -0300

    Prove upper bounds for dense set vs delta indicator
    commit 1c30cd180cd8b34427496a166c06153906419120
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Tue Dec 31 18:27:53 2024 -0300
    
        Finish dense sets and functions upper bounds
    
    commit eac4486327656d9b0db07aa247585cea90fdda1d
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sun Dec 29 13:13:15 2024 -0300
    
        Remove other unbound simps
    
    commit a48583ee6d8625f28f25d8a1b9cceb1f320c4da2
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sun Dec 29 12:49:20 2024 -0300
    
        Remove unbound simps
    
    commit f73c2c3dcbbad0ac3b3b5aa893ed40499b27615e
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sun Dec 29 12:42:49 2024 -0300
    
        Shorten proof
    
    commit 8dd6be5666f2c11551554ffe934c580775e5e2fd
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sat Dec 28 20:00:40 2024 -0300
    
        Add another lower bound proof, remove old ugly proofs
    
    commit d56e9c7fbd23f88c443d471bf0aa3eb17aaa828d
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Thu Dec 26 16:19:17 2024 -0300
    
        update lean and mathlib
    
    commit 1058ccb33c4590964b98c922e9337a60809a8cc1
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Tue Oct 15 10:47:33 2024 -0300
    
        (Almost) complete proof
    
    commit fce39586d5f7980b885d860cda9747a11b4195bc
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sat Oct 12 19:14:59 2024 -0300
    
        Start proof of first lemma lower bound
    
    commit e0f315baf89abd3189873b6fe03affbc1b973f19
    Author: Gabriel Dahia <gdahia@users.noreply.github.com>
    Date:   Sat Oct 12 18:29:55 2024 -0300
    
        Add stub of first lemma

--- FurstenbergSarkozy/Basic.lean
+++ FurstenbergSarkozy/Basic.lean
@@ -22,6 +22,37 @@ def Finset.containsSquareDifference (S : Finset ℕ) : Prop :=
 noncomputable def Finset.indicator {α : Type} [DecidableEq α] (S : Finset α) : α → ℝ :=
   S.toSet.indicator (const α 1)
 
+lemma sum_indicator_supset_le_card' {α β : Type} [DecidableEq α] [DecidableEq β]
+    (S : Finset α) {T : Finset β} {f : β → α} (hf : Set.InjOn f (f ⁻¹' S)) :
+    ∑ x ∈ T, S.indicator (f x) ≤ #S := by
+  simp only [indicator, Set.indicator, mem_coe, const_apply]
+  apply le_trans
+  · apply sum_le_sum
+    · intro x hx
+      apply le_of_eq
+      apply ite_congr (c := x ∈ S.preimage f hf)
+      · simp only [Finset.mem_preimage, mem_coe]
+      · intro
+        rfl
+      · intro
+        rfl
+  rw [sum_ite_mem]
+  simp only [sum_const, nsmul_eq_mul, mul_one, Nat.cast_le]
+  have T_inter_pre_S_subset_pre_S : T ∩ S.preimage f hf ⊆ S.preimage f hf := by simp only [inter_subset_right]
+  apply le_trans
+  · apply card_le_card T_inter_pre_S_subset_pre_S
+  · apply card_le_card_of_injOn
+    · simp only [mem_preimage]
+      intro _ hfS
+      exact hfS
+    · simp only [coe_preimage]
+      assumption
+
+lemma sum_indicator_supset_le_card {α : Type} [DecidableEq α] {S T : Finset α} :
+    ∑ x ∈ T, S.indicator x ≤ #S := by
+  apply sum_indicator_supset_le_card'
+  simp only [Set.preimage_id', Set.InjOn, mem_coe, imp_self, implies_true]
+
 lemma non_zero_countOfSquares_implies_squareDifference (n : ℕ) (S : Finset ℕ)
      : countOfSquares n S.indicator ≠ 0 → S.containsSquareDifference := by
   contrapose!
@@ -50,64 +81,19 @@ lemma non_zero_countOfSquares_implies_squareDifference (n : ℕ) (S : Finset ℕ
     simp only [const_one, mul_eq_zero, Set.indicator_apply_eq_zero, mem_coe, Pi.one_apply,
       one_ne_zero, imp_false]
     right
-    simp at hy
-    simp at hz
+    simp only [mem_Ioc] at hy
+    simp only [mem_Ioc] at hz
     have ypz_pos : 0 < y + z := by omega
     exact (squareDifferenceFree ⟨ y + z, ypz_pos ⟩ x xInS)
 
-noncomputable def maxOfGeneralizedCountOfSquares (n₁ n₂ n₃ : ℕ) :=
-  n₁ * (upperBoundOny n₂) * (upperBoundOnz n₃)
-noncomputable def maxOfCountOfSquares (n : ℕ) := maxOfGeneralizedCountOfSquares n n n
-noncomputable def almostN (n : ℕ) := n - (upperBoundOny n + upperBoundOnz n)^2
-noncomputable def almostMaxOfCountOfSquares (n : ℕ) :=
-  maxOfGeneralizedCountOfSquares (almostN n) n n
+lemma uniform_indicator_vs_uniform_indicator_upper_bound (n : ℕ) (δ : ℝ) :
+    countOfSquares n (δ • (range' n).indicator) ≤ δ ^ 2 * n * (upperBoundOny n) * (upperBoundOnz n) := by
 
-lemma uniform_δ_indicator_at_least_sqr_δ_density_of_countOfSquares_minus_error
-    {n : ℕ} {δ : ℝ}
-    (n_is_large : (upperBoundOny n + upperBoundOnz n)^2 ≤ n) :
-    δ ^ 2 * (almostMaxOfCountOfSquares n) ≤ countOfSquares n (δ • (range' n).indicator) := by
-  simp only [countOfSquares, generalizedCountOfSquares, range', indicator,
-              Pi.smul_apply, smul_eq_mul, coe_Ioc]
-  have almost_sub : Ioc 0 (almostN n) ⊆ Ioc 0 n := by
-    simp only [almostN, Ioc_subset_Ioc_right, Nat.sub_le]
-  refine le_trans (le_of_eq ?_) (sum_le_sum_of_subset_of_nonneg almost_sub ?_)
-  · rw [sum_eq_card_nsmul]
-    rotate_left
-    · intro a ha
-      rw [sum_eq_card_nsmul]
-      intro y hy
-      rw [sum_eq_card_nsmul (b := δ * δ)]
-      intro z hz
-      have cond : a + (y + z) ^ 2 ∈ Set.Ioc 0 n := by
-        simp only [mem_Ioc, Set.mem_Ioc, add_pos_iff] at ha hy hz ⊢
-        refine ⟨Or.inl ha.1, ?_⟩
-        dsimp only [almostN] at ha
-        have hyz : (y + z) ^ 2 ≤ (upperBoundOny n + upperBoundOnz n) ^ 2 := by
-          nlinarith only [hy.2, hz.2]
-        simpa only [tsub_add_cancel_of_le n_is_large] using add_le_add ha.2 hyz
-      have ha : a ∈ Set.Ioc 0 n := by simpa only [mem_Ioc] using almost_sub ha
-      rw [Set.indicator_of_mem ha _, Set.indicator_of_mem cond _]
-      simp only [const_apply, mul_one]
-    · simp only [almostMaxOfCountOfSquares, maxOfGeneralizedCountOfSquares,
-        Nat.cast_mul, Nat.card_Ioc, tsub_zero]
-      ring
-  · intro a _ _
-    apply sum_nonneg
-    intro y _
-    apply sum_nonneg
-    intro z _
-    ring_nf
-    refine mul_nonneg (mul_nonneg (sq_nonneg δ) ?_) ?_ <;>
-      exact Set.indicator_nonneg (by simp) _
+  simp only [countOfSquares, generalizedCountOfSquares, indicator, range',
+    coe_Ioc, const_one, Pi.smul_apply, smul_eq_mul, Nat.cast_mul, Set.indicator,
+    Set.mem_Ioc, Pi.one_apply, mul_ite, mul_one, mul_zero, add_pos_iff, ite_mul,
+    zero_mul, ←ite_and]
 
-lemma uniform_δ_indicator_at_most_sqr_δ_density_of_countOfSquares
-    (n : ℕ) (δ : ℝ) :
-    countOfSquares n (δ • (range' n).indicator) ≤ δ ^ 2 * (maxOfCountOfSquares n) := by
-  simp only [countOfSquares, maxOfCountOfSquares, maxOfGeneralizedCountOfSquares,
-              generalizedCountOfSquares, indicator, range', coe_Ioc, const_one,
-              Pi.smul_apply, smul_eq_mul, Nat.cast_mul, Set.indicator, Set.mem_Ioc,
-              Pi.one_apply, mul_ite, mul_one, mul_zero, add_pos_iff, ite_mul, zero_mul,
-              ←ite_and]
   apply le_trans
   · apply sum_le_card_nsmul
     intro x hx
@@ -126,15 +112,86 @@ lemma uniform_δ_indicator_at_most_sqr_δ_density_of_countOfSquares
     left
     exact sq_nonneg δ
 
+noncomputable def unitInterval' := (Set.Ioc (0 : ℝ) (1 : ℝ))
+
+lemma dense_set_vs_uniform_indicator_upper_bound {n : ℕ} {δ : ℝ} {S : Finset ℕ}
+    (δ_is_density : δ ∈ unitInterval') (S_is_dense : #S = δ * n) :
+    generalizedCountOfSquares n S.indicator (δ • (range' n).indicator) ≤
+    δ ^ 2 * n * (upperBoundOny n) * (upperBoundOnz n) := by
+
+  simp only [generalizedCountOfSquares]
+
+  rw [sum_comm]
+  apply le_trans
+  · apply sum_le_card_nsmul
+    intro y hy
+    rw [sum_comm]
+    apply sum_le_card_nsmul
+    intro z hz
+    apply sum_le_sum
+    · intro x hx
+      apply mul_le_mul_of_nonneg_left
+      · simp only [indicator, range', coe_Ioc, const_one, Pi.smul_apply, Set.indicator,
+          Set.mem_Ioc, add_pos_iff, Pi.one_apply, smul_eq_mul, mul_ite, mul_one, mul_zero]
+        apply le_trans
+        · apply ite_le_sup δ 0
+        · simp only [sup_le_iff]
+          constructor
+          · exact le_rfl
+          · simp only [unitInterval'] at δ_is_density
+            exact δ_is_density.1.le
+      · simp only [indicator, Set.indicator, mem_coe, const_apply]
+        positivity
+  · simp only [range', Nat.card_Ioc, tsub_zero, nsmul_eq_mul]
+    rw [← mul_assoc, mul_comm, mul_assoc]
+    refine mul_le_mul ?_ le_rfl (by positivity) (by positivity)
+    rw [← sum_mul, mul_comm]
+    apply le_trans (mul_le_mul_of_nonneg_left sum_indicator_supset_le_card δ_is_density.1.le)
+    rw [S_is_dense]
+    ring_nf
+    exact le_rfl
+
+lemma uniform_indicator_vs_dense_set_upper_bound {n : ℕ} {δ : ℝ} {S : Finset ℕ}
+    (δ_is_density : δ ∈ unitInterval') (S_is_dense : #S = δ * n) :
+    generalizedCountOfSquares n (δ • (range' n).indicator) S.indicator ≤
+    δ ^ 2 * n * (upperBoundOny n) * (upperBoundOnz n) := by
+
+  simp only [generalizedCountOfSquares]
+
+  rw [sum_comm]
+  apply le_trans
+  · apply sum_le_card_nsmul
+    intro y hy
+    rw [sum_comm]
+    apply sum_le_card_nsmul
+    intro z hz
+
+    have bound_on_uniform_indicator :
+        ∀ x ∈ range' n, ((δ • (range' n).indicator) x) * S.indicator (x + (y + z)^2) ≤ δ * S.indicator (x + (y + z)^2) := by
+      intro x hx
+      gcongr
+      · simp only [indicator, Set.indicator, mem_coe, const_apply]
+        positivity
+      · simp_all only [range', mem_Ioc, indicator, coe_Ioc, const_one, Pi.smul_apply, Set.mem_Ioc,
+          and_self, Set.indicator_of_mem, Pi.one_apply, smul_eq_mul, mul_one, le_refl]
+
+    apply le_trans (sum_le_sum bound_on_uniform_indicator)
+    · rw [← mul_sum, mul_le_mul_left]
+      · apply sum_indicator_supset_le_card'
+        · simp only [Set.InjOn, Set.mem_preimage, mem_coe, add_left_inj, imp_self, implies_true]
+      · simp only [unitInterval', Set.mem_Ioc] at δ_is_density
+        exact δ_is_density.1
+  · simp only [range', Nat.card_Ioc, tsub_zero, S_is_dense, nsmul_eq_mul]
+    ring_nf
+    simp only [le_refl]
+
 -- approach should be the following: do cases. if we have not many fewer than
 -- expected square differences in S, then we are done. otherwise, we can go to a
 -- denser subset by the density increment argument and we show that recursively
 -- calling furstenberg_sarkozy terminates because δ increases and is at most one
 
-noncomputable def unitInterval' := (Set.Ioc (0 : ℝ) (1 : ℝ))
-
 theorem furstenberg_sarkozy :
     ∀ δ ∈ unitInterval', ∃ n₀ : ℕ, ∀ n : ℕ, n ≥ n₀ →
-    ∀ S ⊆ (range' n), δ * n ≤ S.card →
+    ∀ S ⊆ (range' n), δ * n ≤ #S →
     S.containsSquareDifference :=
   sorry
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import FurstenbergSarkozy
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
