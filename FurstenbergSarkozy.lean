79a8acbf32e91f2b91073b83a6f9d66fbd6d8301
Gabriel Dahia <gdahia@users.noreply.github.com>
2025-01-01 11:49:20 -0300

    Add main to target

--- FurstenbergSarkozy.lean
+++ FurstenbergSarkozy.lean
@@ -1,3 +1,4 @@
--- This module serves as the root of the `FurstenbergSarkozy` library.
--- Import modules here that should be built as part of the library.
 import FurstenbergSarkozy.Basic
+
+def main : IO Unit :=
+  IO.println s!"Hello!"
